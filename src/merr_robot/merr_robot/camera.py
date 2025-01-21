e379b4ea6fe97eb0cd2212d80e50ce8f831eee28
rahgirrafi <rahgirrafi@gmail.com>
2025-01-21 06:41:55 +0600

    Image compression and transportation done

--- src/merr_robot/merr_robot/camera.py
+++ src/merr_robot/merr_robot/camera.py
@@ -1,6 +1,7 @@
 import rclpy # Python Client Library for ROS 2
 from rclpy.node import Node # Handles the creation of nodes
 from sensor_msgs.msg import Image # Image is the message type
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
  
@@ -8,7 +9,8 @@ class ImagePublisher(Node):
 
   def __init__(self):
       super().__init__('camera')
-      self.publisher_ = self.create_publisher(Image, 'image_raw', 10)
+      self.publisher_ = self.create_publisher(Image, 'main_camera/image_raw', 25)
+      self.compressed_publisher_ = self.create_publisher(CompressedImage, 'main_camera/image_raw/compressed', 25)
       timer_period = 0.1  # second
       self.timer = self.create_timer(timer_period, self.timer_callback)
       self.cap = cv2.VideoCapture(0)     
@@ -16,13 +18,11 @@ class ImagePublisher(Node):
   
   def timer_callback(self):
       ret, frame = self.cap.read()   
-      #compress image
       encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-      result, frame_compressed = cv2.imencode('.jpg', frame, encode_param)
-
-
+      result, compressedFrame = cv2.imencode('.jpg', frame, encode_param)
       if ret == True:
-        self.publisher_.publish(self.br.cv2_to_compressed_imgmsg(frame_compressed))
+        self.publisher_.publish(self.br.cv2_to_imgmsg(frame)) 
+        self.compressed_publisher_.publish(self.br.cv2_to_compressed_imgmsg(compressedFrame))
         self.get_logger().info('Publishing video frame')
  
 def main(args=None):
