61be76e16cdb9f113d969caf7c6850a90d9aed0e
rahgirrafi <rahgirrafi@gmail.com>
2025-01-22 03:09:57 +0600

    approching custom camera node

--- src/merr_robot/merr_robot/camera.py
+++ src/merr_robot/merr_robot/camera.py
@@ -10,7 +10,6 @@ class ImagePublisher(Node):
   def __init__(self):
       super().__init__('camera')
       self.publisher_ = self.create_publisher(Image, 'main_camera/image_raw', 25)
-      self.compressed_publisher_ = self.create_publisher(CompressedImage, 'main_camera/image_raw/compressed', 25)
       timer_period = 0.1  # second
       self.timer = self.create_timer(timer_period, self.timer_callback)
       self.cap = cv2.VideoCapture(0)     
@@ -18,11 +17,8 @@ class ImagePublisher(Node):
   
   def timer_callback(self):
       ret, frame = self.cap.read()   
-      encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-      result, compressedFrame = cv2.imencode('.jpg', frame, encode_param)
       if ret == True:
         self.publisher_.publish(self.br.cv2_to_imgmsg(frame)) 
-        self.compressed_publisher_.publish(self.br.cv2_to_compressed_imgmsg(compressedFrame))
         self.get_logger().info('Publishing video frame')
  
 def main(args=None):
